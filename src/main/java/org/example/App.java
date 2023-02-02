e340b8edf1736fbee11fd81f8336f57c1eb75743
manosriramMSRgit <manosriram55555@gmail.com>
2023-02-02 11:25:11 +0530

    hello world

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello from "+ name);
+    }
+}
